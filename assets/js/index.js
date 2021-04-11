faa7deb4e7febc41a9a74c0a13fa5008f5f5ad8a
gemmacollinge <gemmacollinge_71@hotmail.com>
2021-04-11 15:49:24 +0100

    rendered restaurant cards on load

--- assets/js/index.js
+++ assets/js/index.js
@@ -54,7 +54,7 @@ const restaurants = [
     address: "8 Vernon Street, Stockport, SK1 1TY",
     phoneNumber: "0123 222 333",
     logoUrl:
-      "https://d30v2pzvrfyzpo.cloudfront.net/uk/images/restaurants/10230.gif",
+      "https://d30v2pzvrfyzpo.cloudfront.net/uk/images/restaurants/126030.gif",
     cuisine: "Fish & Chips",
     distance: 1.8,
     stars: 4.7,
@@ -72,9 +72,46 @@ const getFilteredRestaurants = (searchTerm) => {
   return restaurants.filter(callback);
 };
 
-const renderRestaurants = () => {
+const onSearch = () => {
   const restaurantName = $("#search").val();
   const filteredRestaurants = getFilteredRestaurants(restaurantName);
 };
 
-$("#search").on("input", renderRestaurants);
+const renderRestaurantCards = (restaurants) => {
+  const container = $("#restaurants-container");
+
+  const renderCard = (eachRestaurant) => {
+    const card = `<div class="card text-center m-2" style="width: 18rem">
+    <div class="card-header">
+      <img
+        src="${eachRestaurant.logoUrl}"
+        class="img-thumbnail img-logo"
+        alt="${eachRestaurant.name}"
+      />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${eachRestaurant.name}</h5>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+        ${eachRestaurant.address}
+        </li>
+        <li class="list-group-item">${eachRestaurant.phoneNumber}</li>
+        <li class="list-group-item">${eachRestaurant.distance} miles</li>
+        <li class="list-group-item fs-6 text-muted">
+        ${eachRestaurant.stars} stars (${eachRestaurant.name} reviews)
+        </li>
+      </ul>
+    </div>
+  </div>`;
+    container.append(card);
+  };
+
+  restaurants.forEach(renderCard);
+};
+
+const onLoad = () => {
+  renderRestaurantCards(restaurants);
+};
+
+$("#search").on("input", onSearch);
+$(document).ready(onLoad);
