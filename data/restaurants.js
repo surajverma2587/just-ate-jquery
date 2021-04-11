fd5f3bd412abb51c061c1cf74850e029ab8051ad
Suraj Verma <surajverma@rentalcars.com>
2021-04-11 13:23:42 +0100

    add sample data

---
+++ data/restaurants.js
@@ -0,0 +1,25 @@
+const restaurants = [
+  {
+    name: "Halo",
+    address: "109 London Road, Stockport, SK7 4HH",
+    phoneNumber: "0123 456 789",
+    logoUrl:
+      "https://d30v2pzvrfyzpo.cloudfront.net/uk/images/restaurants/50736.gif",
+    cuisine: "Burgers",
+    distance: 2.5,
+    stars: 4.3,
+    reviews: 350,
+    discount: 10,
+  },
+  {
+    name: "Sagar Tandoori",
+    address: "381 Buxton Road, Stockport, SK2 7EY",
+    phoneNumber: "0123 456 111",
+    logoUrl:
+      "https://d30v2pzvrfyzpo.cloudfront.net/uk/images/restaurants/10230.gif",
+    cuisine: "Indian",
+    distance: 1.5,
+    stars: 4.2,
+    reviews: 1341,
+  },
+];
